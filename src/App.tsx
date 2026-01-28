48e407762f888e7ef06e9a03ebe9dc7666b2f644
lf-kali <cbjk.corp@gmail.com>
2026-01-28 20:06:42 -0300

    merge

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import DeletarPlano from './components/planos/deleteplano/DeletePlano'
 
 function App() {
   return (
+    <>
     <BrowserRouter>
       <div
         aria-hidden="true"
@@ -45,6 +46,7 @@ function App() {
         <Footer />
       </div>
     </BrowserRouter>
+    </>
   )
 }
 
